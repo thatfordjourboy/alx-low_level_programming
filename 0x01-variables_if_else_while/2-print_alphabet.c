541148ac766a712a36c3997443c76c8bf8edca1b
Eleazer <quaysoneleazer@gmail.com>
2022-12-12 13:54:58 -0800

    alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,8 +6,10 @@
 int main(void)
 {
 	char letter;
-	for (letter = 'a'; letter = 'z'; letter ++)
+
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
+
 	putchar('\n');
 	return (0);
 }
