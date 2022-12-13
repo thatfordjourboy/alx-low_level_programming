6a173f7b25ccec82d2213eb74e61395118ed505d
Eleazer <quaysoneleazer@gmail.com>
2022-12-12 20:15:24 -0800

    inventing, brains, that kind of stuff

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible different combinations of two digits
+ * separated by , and followed by a space
+ * the two digits must be different
+ * Return: Always 0
+ */
+int main(void)
+{
+	int digit_1, digit_2;
+
+	for (digit_1 = 0; digit_1 < 9; digit_1++)
+	{
+		for (digit_2 = digit_1 + 1; digit_2 < 10; digit_2++)
+		{
+			putchar((digit_1 % 10) + '0');
+			putchar((digit_2 % 10) + '0');
+			if (digit_1 == 8 && digit_2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
