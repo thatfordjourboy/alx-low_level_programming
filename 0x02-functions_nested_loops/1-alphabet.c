2d6898cf9760ea51308503d844dbe1d31ed702e8
Eleazer <quaysoneleazer@gmail.com>
2022-12-14 20:00:12 -0800

    insomnia and sleep type thing

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,8 @@
 #include "main.h"
-
 /**
  * print_alphabet - check the code for ALX School students.
- *
  * Return: Always 0.
  */
-
 void print_alphabet(void)
 {
 	char alp = 'a';
@@ -14,8 +11,6 @@ void print_alphabet(void)
 	{
 		_putchar (alp);
 		alp++;
-
 	}
 	_putchar ('\n');
-
 }
