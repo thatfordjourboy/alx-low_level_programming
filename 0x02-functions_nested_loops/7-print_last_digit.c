836919ba87db2a684113a4960b9efc94df658b1f
Eleazer <quaysoneleazer@gmail.com>
2022-12-14 20:34:33 -0800

    colors, digits, notes. do what you want with them

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_last_digit - prints the last digit of a number
  * @n: the int to extract the last digit from
@@ -7,12 +6,13 @@
  */
 int print_last_digit(int n)
 {
-int a;
-if (n < 0)
-n = -n;
-a = n % 10;
-if (a < 0)
-a = -a;
-_putchar(a + '0');
-return (a);
+	int a;
+
+	if (n < 0)
+		n = -n;
+	a = n % 10;
+	if (a < 0)
+		a = -a;
+	_putchar(a + '0');
+	return (a);
 }
