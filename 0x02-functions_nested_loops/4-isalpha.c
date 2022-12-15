7c0dc8541c3aa233013cc0360baec96016c70767
Eleazer <quaysoneleazer@gmail.com>
2022-12-14 20:19:24 -0800

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,9 @@
 #include "main.h"
-
 /**
  * _isalpha - check the code for ALX School students.
- *
- * @c: is c an integer argument
- *
+ * @c: is an integer argument
  * Return: Always 0.
  */
-
 int _isalpha(int c)
 {
 
